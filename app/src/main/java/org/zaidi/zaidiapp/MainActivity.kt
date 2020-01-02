3f8578052bf79a34518ffd8776d44ec9852f2c69
erickogi <erickogi14@gmail.com>
2020-01-02 16:42:07 +0300

    Initial Commit

---
+++ app/src/main/java/org/zaidi/zaidiapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.zaidi.zaidiapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
